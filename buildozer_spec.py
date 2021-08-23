bbdcd5d9af2518fea13e586bf112ddf82eac158c
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-23 21:08:53 +0300

    Update buildozer_spec.py

--- buildozer_spec.py
+++ buildozer_spec.py
@@ -2,18 +2,18 @@ f = open("buildozer.spec", 'r')
 s = f.read()
 f.close()
 
-s = s.replace("log_level = 1", "log_level = 2")
-s = s.replace("# android.accept_sdk_license = False", "android.accept_sdk_license = True")
-s = s.replace("#android.permissions = INTERNET", "android.permissions = INTERNET,CAMERA,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,RECORD_AUDIO,ACCESS_FINE_LOCATION,ACCESS_COARSE_LOCATION")
-s = s.replace("requirements = python3,kivy", "requirements = libffi,imutils,opencv,numpy,cffi,python3==3.7.9,hostpython3==3.7.9,kivy==master,kivymd==0.104.2,sdl2_ttf==2.0.15,pillow,requests,urllib3,charset-normalizer,idna,plyer,mediapipe")
-s = s.replace("#p4a.branch = master", "p4a.branch = develop")
-s = s.replace("osx.python_version = 3", "osx.python_version = 3.7.9")
-s = s.replace("android.arch = armeabi-v7a", "android.arch = x86-64")
-s = s.replace("#android.add_libs_x86 = libs/android-x86/*.so", "android.add_libs_x86_64 = libs/android-x86-64/*.so")
-#s = s.replace("android.arch = armeabi-v7a", "android.arch = arm64-v8a")
-#s = s.replace("#android.add_libs_arm64_v8a = libs/android-v8/*.so", "android.add_libs_arm64_v8a = libs/android-v8/*.so")
 
 s = s.replace("#android.api = 27", "android.api = 30")
+s = s.replace("log_level = 1", "log_level = 2")
+s = s.replace("# android.accept_sdk_license = False", "android.accept_sdk_license = True")
+s = s.replace("requirements = python3,kivy", "requirements = python3==3.7.9,hostpython3==3.7.9,kivy==master,mediapipe")
+s = s.replace("#p4a.branch = master", "p4a.branch = develop")
+s = s.replace("osx.python_version = 3", "osx.python_version = 3.7.9")
+
+s = s.replace("android.arch = armeabi-v7a", "android.arch = arm64-v8a")
+#s = s.replace("android.arch = armeabi-v7a", "android.arch = x86-64")
+#s = s.replace("#android.add_libs_x86 = libs/android-x86/*.so", "android.add_libs_x86_64 = libs/android-x86-64/*.so")
+
 
 f = open("buildozer.spec", 'w')
 f.write(s)
