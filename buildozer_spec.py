c272c20b035af7fc07caa839cf5759359dca8a79
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-24 12:02:33 +0300

    Update buildozer_spec.py

--- buildozer_spec.py
+++ buildozer_spec.py
@@ -6,7 +6,7 @@ f.close()
 s = s.replace("#android.api = 27", "android.api = 30")
 s = s.replace("log_level = 1", "log_level = 2")
 s = s.replace("# android.accept_sdk_license = False", "android.accept_sdk_license = True")
-s = s.replace("requirements = python3,kivy", "requirements = python3==3.7.9,hostpython3==3.7.9,kivy==master,mediapipe")
+s = s.replace("requirements = python3,kivy", "requirements = python3==3.7.9,hostpython3==3.7.9,kivy==master") #,mediapipe
 s = s.replace("#p4a.branch = master", "p4a.branch = develop")
 s = s.replace("osx.python_version = 3", "osx.python_version = 3.7.9")
 
